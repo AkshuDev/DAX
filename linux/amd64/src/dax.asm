d5ea8488f3c3ad1ab8706fda738eb1d762ca9a66
AkshuDev <148709451+AkshuDev@users.noreply.github.com>
2025-07-22 16:38:53 +0000

    Updated AMD64 Linux

--- linux/amd64/src/dax.asm
+++ linux/amd64/src/dax.asm
@@ -10,12 +10,19 @@ extern dax_strcpy
 
 section .data
 usage_error db "Usage: dax <input> <output> <-[OPTIONS]>", ENDL, NULL_T
+
 starting_dax db "Starting dax...", ENDL, NULL_T
-DaXing_file db "daXing file "
-DaXing_to db " to "
+DaXing_file db "daXing file ", NULL_T
+DaXing_to db " to ", NULL_T
+
+read_error db "ERROR: Unable to read <input> file!", ENDL, NULL_T
+
 endline db "", ENDL, NULL_T
 
 section .bss
+fnIn resb MAX_FILE_BUF
+fnOut resb MAX_FILE_BUF
+
 fdIn resb MAX_FILE_BUF
 fdOut resb MAX_FILE_BUF
 
@@ -25,41 +32,79 @@ section .text
 global _start
 
 _start: ; rbx, r12, r13, r14, r15 (calle saved)
-    ; Parse Args
-    pop rbx ; argc
+    ; Parse Args (do not pop off the stack to disturb the alignment)
+    mov rsi, [rsp] ; argc
 
-    cmp rbx, 3
+    cmp rsi, 3
     jl dax_usage_error
 
-    pop rsi
-    pop rsi ; src
-    lea rdi, [rel fdIn] ; dest
+    mov rsi, [rsp + 8] ; useless tho (argv[0])
+    mov rsi, [rsp + 16] ; src (argv[1], file input)
+    lea rdi, [rel fnIn] ; dest
     call dax_strcpy
 
-    pop rsi ; src
-    lea rdi, [rel fdOut] ; dest
+    mov rsi, [rsp + 24] ; src (argv[2], file output)
+    lea rdi, [rel fnOut] ; dest
     call dax_strcpy
 
     lea rdi, [rel starting_dax]
     call dax_printf
 
-    ; print -> daXing file [fdIn] to [fdOut]\n\0
+    ; print -> daXing file [fnIn] to [fnOut]\n\0
     lea rdi, [rel DaXing_file]
     call dax_printf
 
-    lea rdi, [rel fdIn]
+    lea rdi, [rel fnIn]
     call dax_printf
 
     lea rdi, [rel DaXing_to]
     call dax_printf
 
-    lea rdi, [rel fdOut]
+    lea rdi, [rel fnOut]
     call dax_printf
 
     lea rdi, [rel endline]
     call dax_printf
 
+    ; Now we begin here!
+    mov rax, 2
+    lea rdi, [rel fnIn]
+    mov rsi, O_RDONLY
+    mov rdx, MODE_644
+    syscall
+
+    mov [rel fdIn], rax ; Save file descriptor
+
+    ; now read
+    mov rax, 0
+    mov rdi, [rel fdIn]
+    lea rsi, [rel buffer]
+    mov rdx, MAX_FILE_CONTENTS_BUF
+    syscall
+
+    cmp rax, 0 ; if the read contents were zero
+    jl dax_read_error
+
+    ; now close
+    mov rax, 3
+    mov rdi, [rel fdIn]
+    syscall
+
+    ; Now we hand over to 
+    ; Just in case the jmp fails
     mov rdi, 0
+    jmp exit
+
+dax_read_error:
+    ; Close the file first
+    mov rax, 3
+    mov rdi, [rel fdIn]
+    syscall ; Close
+
+    lea rdi, [rel read_error]
+    call dax_printf ; Print Error
+
+    mov rdi, UNABLE_TO_READ_FILE_ERROR
     jmp exit
 
 dax_usage_error:
